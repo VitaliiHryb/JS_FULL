3f5f679ac84d5244688e1313b1b565f53890c018
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-28 17:59:41 +0300

    unsorted array => sorted array

--- lesson_7/class/task-2/index.js
+++ lesson_7/class/task-2/index.js
@@ -9,8 +9,8 @@
 // arraywill not change
 const sortDesc = numbers => {
     const copy = numbers.slice();
-    numbers.sort((a, b) => b - a);
-    return numbers;
+    copy.sort((a, b) => b - a);
+    return copy;
 }
 
 const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
