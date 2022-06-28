ed43688344b3162b44c294d2a47c1cf1f8c3f508
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-28 17:58:47 +0300

    unsorted array => sorted array

--- lesson_7/class/task-2/index.js
+++ lesson_7/class/task-2/index.js
@@ -1,9 +1,19 @@
-// task-1 short
-const getSpecialNumbers = numbers =>
- numbers.filter(num => num % 3 === 0);
+// unsorted array => sorted array
 
+//array will change
+/*const sortArray = numbers => {
+    numbers.sort((a, b) => b - a);
+    return numbers;
+}*/
+
+// arraywill not change
+const sortDesc = numbers => {
+    const copy = numbers.slice();
+    numbers.sort((a, b) => b - a);
+    return numbers;
+}
 
 const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
 
-console.log(getSpecialNumbers(arr));
 console.log(arr);
+console.log(sortDesc(arr));
