bd974ff0b0cdeb0b06cbc9d5d25bcfeef4605668
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-28 17:30:41 +0300

    task-1 short

--- lesson_7/class/task-1/index.js
+++ lesson_7/class/task-1/index.js
@@ -1,4 +1,14 @@
 // фильтрацияя массива
+// task-1 short
+const getSpecialNumbers = numbers =>
+ numbers.filter(num => num % 3 === 0);
+
+
+const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
+
+console.log(getSpecialNumbers(arr));
+console.log(arr);
+
 // изменяет массив
 /*const getSpecialNumbers = numbers => {
     let getSpecialNumbers = [];
@@ -14,8 +24,8 @@
 
 console.log(getSpecialNumbers([1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9]));*/
 
-// не меняет массив
-
+// task-1 long version
+/*
 const getSpecialNumbers = numbers => {
     let SpecialNumbers = [];
 
@@ -32,4 +42,4 @@ const getSpecialNumbers = numbers => {
 
 const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
 
-console.log(getSpecialNumbers(arr));
+console.log(getSpecialNumbers(arr));*/
---
+++ lesson_7/class/task-2/index.js
@@ -0,0 +1,9 @@
+// task-1 short
+const getSpecialNumbers = numbers =>
+ numbers.filter(num => num % 3 === 0);
+
+
+const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
+
+console.log(getSpecialNumbers(arr));
+console.log(arr);
