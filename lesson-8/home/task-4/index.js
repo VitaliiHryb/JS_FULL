0ece5513e3bae9471cf0b7b4889c1bb3d24b8c3c
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-30 01:54:00 +0300

    pick props

--- lesson-8/home/task-4/index.js
+++ lesson-8/home/task-4/index.js
@@ -1,45 +1,20 @@
-// фильтрацияя массива
-// task-1 short
-const getSpecialNumbers = numbers =>
- numbers.filter(num => num % 3 === 0);
+/* eslint-disable no-restricted-syntax */
 
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
 
-const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
-
-console.log(getSpecialNumbers(arr));
-console.log(arr);
-
-// изменяет массив
-/*const getSpecialNumbers = numbers => {
-    let getSpecialNumbers = [];
-
-    for(let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 3 === 0) {
-            getSpecialNumbers.push(numbers[i]);
-        }
+const pickProps = (obj, props) => {
+  const result = {};
+  for (const key in obj) {
+    if (props.includes(key)) {
+      result[key] = obj[key];
     }
-
-    return getSpecialNumbers;
+  }
+  return result;
 };
 
-console.log(getSpecialNumbers([1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9]));*/
-
-// task-1 long version
-/*
-const getSpecialNumbers = numbers => {
-    let SpecialNumbers = [];
-
-    function checkIfSpecialNumber(num) {
-        if (num % 3 === 0) {
-            SpecialNumbers.push(num);
-        }
-    }
-
-    numbers.forEach(checkIfSpecialNumber);
-
-    return SpecialNumbers;
-};
-
-const arr = [1,2,3,4,5,6,35,34,3,4,34,34,4,3,4,3,434,3,3,5,6,9];
-
-console.log(getSpecialNumbers(arr));*/
+// examples
+pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c']); // ==> { a: 1, c: 3 }
+pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex']); // ==> { a: 1, c: 3 }
