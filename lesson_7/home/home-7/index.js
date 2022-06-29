4391b32402ca8fd350c502e5724022d178caf877
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-29 21:07:37 +0300

    names filter

---
+++ lesson_7/home/home-7/index.js
@@ -0,0 +1,12 @@
+// names filter
+
+const filterNames = (arr, text) => arr.filter(arr => arr.includes(text) && arr.length > 5);
+
+// const testArr = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// console.log(filterNames(testArr, 'e'));
+
+// const testArr2 = ['spray', 'limit', 'elite'];
+// console.log(filterNames(testArr2, 'e'));
+
+// const testArr3 = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'new'];
+// console.log(filterNames(testArr3, 'e'));
