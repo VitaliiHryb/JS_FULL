156ff107d3a527df93265c53861acbb5fccfcf9b
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-30 21:07:57 +0300

    Adding a property to an object

--- lesson-9/home/home-2/index.js
+++ lesson-9/home/home-2/index.js
@@ -1,32 +1,65 @@
-/* eslint-disable no-restricted-syntax */
+// Добавление свойства в объект
+/* eslint-disable prefer-object-spread */
+/* eslint-disable no-param-reassign */
 
-/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
- * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
- * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+/* В решения этой задачи используется метод Object.assign. В реальных проектах для такой задачи
+ * лучше использовать spread опертор - это самый современный подход
+ *
+ * Так же плохой подход - мутировать входящие параметры функции
+ *
+ * Задачу мы делаем для практики и демонстрационных целей, поэтому чтобы eslint не ругался на эту ошибку,
+ * для этой задачи он отключен аннотацией eslint-disable
  * */
 
-// используй for..in цикл.
-
-// пройтись циклом по обьекту
-// проверить условие value >= 18
-// записать в обьект key : velue удовлетворяющие условие
-// вернуть новый обьект
-
-const getAdults = obj => {
-  let filterObj = {};
-
-  const isAdult = old => old >= 18;
-
-  for (const key in obj) {
-    if (isAdult(obj[key])) {
-      filterObj[key] = obj[key];
-      // console.log(filterObj);
-    }
-  }
-  return filterObj;
-  //console.log(filterObj);
+const addPropertyV1 = (userData, userId) => {
+  userData.id = userId;
+  return userData;
 };
 
+const addPropertyV2 = (userData, userId) => Object.assign(userData, { id: userId });
+
+const addPropertyV3 = (userData, userId) => Object.assign({}, userData, { id: userId });
+
+const addPropertyV4 = (userData, userId) => Object.assign({ ...userData }, { id: userId });
+
 // examples
-console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
-console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+const user = {
+  name: 'Sam',
+};
+
+// console.log(addPropertyV1(user, '1234567')); // ==> { name: 'Sam', id: '1234567' }
+// console.log(addPropertyV2(user, '1234567')); // ==> { name: 'Sam', id: '1234567' }
+// console.log(addPropertyV3(user, '1234567')); // ==> { name: 'Sam', id: '1234567' }
+// console.log(addPropertyV4(user, '1234567')); // ==> { name: 'Sam', id: '1234567' }
+
+addPropertyV1(user, '1234567'); // ==> { name: 'Sam', id: '1234567' }
+addPropertyV2(user, '1234567'); // ==> { name: 'Sam', id: '1234567' }
+addPropertyV3(user, '1234567'); // ==> { name: 'Sam', id: '1234567' }
+addPropertyV4(user, '1234567'); // ==> { name: 'Sam', id: '1234567' }
+
+// function addPropertyV1(userData, userId) {
+//   userData.id = userId;
+//   console.log(userData);
+// }
+
+// function addPropertyV2(userData, userId) {
+//   const indus = { id: userId };
+//   const addInNewObj = Object.assign(userData, indus);
+//   console.log(addInNewObj);
+//   //return addInNewObj.userId;
+// }
+
+// function addPropertyV3(userData, userId) {
+//   const indus = { id: userId };
+//   const addInNewObj = Object.assign({}, userData, indus);
+//   console.log(addInNewObj);
+//   //return addInNewObj.userId;
+// }
+
+// function addPropertyV4(userData, userId) {
+//   const indus = { id: userId };
+//   const copyObj = { ...userData };
+//   const addInNewObj = Object.assign(copyObj, indus);
+//   console.log(addInNewObj);
+//   //return addInNewObj.userId;
+// }
