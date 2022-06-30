d2cd52d0c40682f95065251642a9ba7f29be24ac
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-30 19:11:38 +0300

    Iterate object keys

--- lesson-9/home/home-1/index.js
+++ lesson-9/home/home-1/index.js
@@ -1,12 +1,11 @@
+// Iterate object keys
 // use Object.keys
 //  console.log all values of obj
 /**
  * @param {object} obj
  * @return {undefined}
  */
-function getKeys(obj) {
-  // put you code here
-}
+const getKeys = obj => Object.keys(obj).forEach(key => console.log(key));
 
 // examples
 getKeys({ name: 'Denis', city: 'Warsaw', englishLevel: 'A1' });
@@ -14,3 +13,8 @@ getKeys({ name: 'Denis', city: 'Warsaw', englishLevel: 'A1' });
 // name
 // city
 // englishLevel
+
+// function getKeys(obj) {
+//   const objKeys = Object.keys(obj);
+//   objKeys.forEach(key => console.log(key));
+// }
