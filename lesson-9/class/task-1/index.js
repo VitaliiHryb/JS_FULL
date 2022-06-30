018ead21d38f1cc340f5e3255ef3f4d6526916c8
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-30 15:53:04 +0300

    keys, values, entries

--- lesson-9/class/task-1/index.js
+++ lesson-9/class/task-1/index.js
@@ -1,32 +1,34 @@
-/* eslint-disable no-restricted-syntax */
-
-/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
- * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
- * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
- * */
-
-// используй for..in цикл.
+// Object - keys(), values(), entries().
 
 // пройтись циклом по обьекту
 // проверить условие value >= 18
 // записать в обьект key : velue удовлетворяющие условие
 // вернуть новый обьект
 
-const getAdults = obj => {
-  let filterObj = {};
+const getAdults = usersObj => {
+  const usersArray = Object.entries(usersObj);
+  const filteredUserArray = usersArray.filter(user => user[1] >= 18);
 
-  const isAdult = old => old >= 18;
+  const usersNames = filteredUserArray.map(user => user[0]);
 
-  for (const key in obj) {
-    if (isAdult(obj[key])) {
-      filterObj[key] = obj[key];
-      // console.log(filterObj);
-    }
-  }
-  return filterObj;
-  //console.log(filterObj);
+  return usersNames;
 };
 
 // examples
-console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe', Bob: 18 }
 console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+
+// const getAdults = obj => {
+//   let filterObj = {};
+
+//   const isAdult = old => old >= 18;
+
+//   for (const key in obj) {
+//     if (isAdult(obj[key])) {
+//       filterObj[key] = obj[key];
+//       // console.log(filterObj);
+//     }
+//   }
+//   return filterObj;
+//   //console.log(filterObj);
+// };
