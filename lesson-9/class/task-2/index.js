fd61ee0fe8245d428716e8e6a869fcc35f8fd323
VitaliiHryb <vitaliigryb@ukr.net>
2022-06-30 17:39:56 +0300

    copy Object other ways

--- lesson-9/class/task-2/index.js
+++ lesson-9/class/task-2/index.js
@@ -6,3 +6,37 @@ const copyObj = obj => Object.assign({}, obj);
 // examples
 console.log(copyObj({ 'John Doe': 19, Tom: 17, Bob: 18 }));
 console.log(copyObj({ Ann: 56, Andrey: 7 }));
+
+// the same but copy only on the first lvl
+// copy Object with spred operator [...res]
+
+const copy = obj1 => {
+  const user = { ...obj1 };
+  return user;
+};
+
+console.log(copy({ 'John Doe': 19, Tom: 17, Bob: 18 }));
+console.log(copy({ Ann: 56, Andrey: 7 }));
+
+// destructorisation
+const someObj = {
+  'John Doe': 19,
+  Tom: 17,
+  Bob: 18,
+  Ann: 56,
+};
+const { Bob } = someObj;
+
+console.log(Bob); // ==> 18;
+
+// rename with destructirisation
+const { Bob: myName } = someObj;
+
+console.log(myName); // ==> 18;
+
+// add key: value but not in obj (dont understand how it works)
+
+const { surname = 'default', ...restProps } = someObj;
+
+console.log(surname); // ==> default;
+console.log(restProps); // ==> {John Doe: 19, Tom: 17, Bob: 18, Ann: 56};
--- lesson-9/home/home-1/index.js
+++ lesson-9/home/home-1/index.js
@@ -1,32 +1,16 @@
-/* eslint-disable no-restricted-syntax */
-
-/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
- * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
- * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
- * */
-
-// используй for..in цикл.
-
-// пройтись циклом по обьекту
-// проверить условие value >= 18
-// записать в обьект key : velue удовлетворяющие условие
-// вернуть новый обьект
-
-const getAdults = obj => {
-  let filterObj = {};
-
-  const isAdult = old => old >= 18;
-
-  for (const key in obj) {
-    if (isAdult(obj[key])) {
-      filterObj[key] = obj[key];
-      // console.log(filterObj);
-    }
-  }
-  return filterObj;
-  //console.log(filterObj);
-};
+// use Object.keys
+//  console.log all values of obj
+/**
+ * @param {object} obj
+ * @return {undefined}
+ */
+function getKeys(obj) {
+  // put you code here
+}
 
 // examples
-console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
-console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+getKeys({ name: 'Denis', city: 'Warsaw', englishLevel: 'A1' });
+// prints ===>
+// name
+// city
+// englishLevel
