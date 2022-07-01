448c50e8bb93734f87af1f683bfeb52b009a9e82
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-01 19:42:31 +0300

    check numbers

--- lesson-10/class/task-1/index.js
+++ lesson-10/class/task-1/index.js
@@ -1,39 +1,96 @@
-// Object - keys(), values(), entries().
+// Проверка чисел
+const getFiniteNumbers = arr => arr.filter(Number.isFinite);
 
-// пройтись циклом по обьекту
-// проверить условие value >= 18
-// записать в обьект key : velue удовлетворяющие условие
-// вернуть новый обьект
+const getFiniteNumbersV2 = arr => arr.filter(isFinite);
 
-const getAdults = usersObj =>
-  Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
+const getNaN = arr => arr.filter(Number.isNaN);
 
-// const getAdults = usersObj => {
-//   const result = Object.entries(usersObj);
-//   const filteredUserArray = result.filter(user => user[1] >= 18);
+const getNaNV2 = arr => arr.filter(isNaN);
 
-//   const usersNames = filteredUserArray.map(user => user[0]);
+const getIntegers = arr => arr.filter(Number.isInteger);
 
-//   return usersNames;
+console.log(getFiniteNumbers([12, 0, -5, '33', [], Infinity, -54.4]));
+console.log(getFiniteNumbersV2([12, 0, -5, '33', [], Infinity, -54.4]));
+console.log(getNaN([12, 0, -5, '33', [], Infinity, -54.4]));
+console.log(getNaNV2([12, 0, -5, '33', [], Infinity, -54.4]));
+console.log(getIntegers([12, 0, -5, '33', [], Infinity, -54.4]));
+
+// Number.isNaN('text');
+// isNaN('text');
+// isNaN(NaN);
+// Number.isNaN(NaN);
+
+// Number.isFinite('17');
+// isFinite('27');
+
+// parseInt(' 17.17text');
+// Number.parseInt(' 17.17text');
+// Number.parseInt('seventeen');
+
+// parseFloat(' 17.17text');
+// Number.parseFloat(' 17.17text');
+// Number.parseFloat('seventeen');
+
+// Number.isInteger(17);
+// Number.isInteger(17.0);
+// Number.isInteger(17.17);
+// Number.isInteger('17');
+// Number.isInteger(Infinity);
+// Number.isInteger(NaN);
+// Number.isInteger(undefined);
+// Number.isInteger(null);
+
+// // 1.
+// const getFiniteNumbers = arr => {
+//   const result = [];
+//   arr.forEach(num => {
+//     if (Number.isFinite(num)) {
+//       result.push(num);
+//     }
+//     return result;
+//   });
 // };
 
-// examples
-console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe', Bob: 18 }
-console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
-
-// const getAdults = obj => {
-//   let filterObj = {};
-
-//   const isAdult = old => old >= 18;
-
-//   for (const key in obj) {
-//     if (isAdult(obj[key])) {
-//       filterObj[key] = obj[key];
-//       // console.log(filterObj);
+// // 2.
+// const getFiniteNumbersV2 = arr => {
+//   const result = [];
+//   arr.forEach(num => {
+//     if (num.isFinite) {
+//       result.push(num);
 //     }
-//   }
-//   return filterObj;
-//   //console.log(filterObj);
+//     return result;
+//   });
+// };
+
+// // 3.
+// const getNaN = arr => {
+//   const result = [];
+//   arr.forEach(num => {
+//     if (Number.isNaN(num)) {
+//       result.push(num);
+//     }
+//     return result;
+//   });
+// };
+
+// // 4.
+// const getNaNV2 = arr => {
+//   const result = [];
+//   arr.forEach(num => {
+//     if (isNaN(num)) {
+//       result.push(num);
+//     }
+//     return result;
+//   });
+// };
+
+// // 5.
+// const getIntegers = arr => {
+//   const result = [];
+//   arr.forEach(num => {
+//     if (Number.isInteger(num)) {
+//       result.push(num);
+//     }
+//     return result;
+//   });
 // };
