daa06daa20f6e93e4b5677dda42f727fea9764f8
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-01 17:25:13 +0300

    js L10 added

---
+++ lesson-10/class/task-4/index.js
@@ -0,0 +1,39 @@
+// Object - keys(), values(), entries().
+
+// пройтись циклом по обьекту
+// проверить условие value >= 18
+// записать в обьект key : velue удовлетворяющие условие
+// вернуть новый обьект
+
+const getAdults = usersObj =>
+  Object.entries(usersObj)
+    .filter(user => user[1] >= 18)
+    .map(user => user[0]);
+
+// const getAdults = usersObj => {
+//   const result = Object.entries(usersObj);
+//   const filteredUserArray = result.filter(user => user[1] >= 18);
+
+//   const usersNames = filteredUserArray.map(user => user[0]);
+
+//   return usersNames;
+// };
+
+// examples
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe', Bob: 18 }
+console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+
+// const getAdults = obj => {
+//   let filterObj = {};
+
+//   const isAdult = old => old >= 18;
+
+//   for (const key in obj) {
+//     if (isAdult(obj[key])) {
+//       filterObj[key] = obj[key];
+//       // console.log(filterObj);
+//     }
+//   }
+//   return filterObj;
+//   //console.log(filterObj);
+// };
