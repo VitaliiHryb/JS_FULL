63f139e8669f35bd7df2a7689dfada1fe6ebe5d2
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-04 19:19:10 +0300

    Reverse String

--- lesson-12/home-1/index.js
+++ lesson-12/home-1/index.js
@@ -1,23 +1,30 @@
-// Count occurences
+// Reverse String
 
-const countOccurrences = (text, str) => {
-  if (str === '') {
+// input: string
+// output:
+
+// strinng is actually an array
+// Step 1. Use the split() method to return a new array
+// Step 2. Use the reverse() method to reverse the new created array
+// Step 3. Use the join() method to join all elements of the array into a string
+// Step 4. Return the reversed string
+
+const reverseString = str => {
+  if (typeof str === 'string') {
+    return str.split('').reverse().join('');
+  } else {
     return null;
   }
-
-  let startPosition = -1;
-  let count = 0;
-  while (true) {
-    startPosition = text.indexOf(str, startPosition + 1);
-    if (startPosition === -1) {
-      break;
-    }
-
-    count += 1;
-  }
-  return count;
 };
 
-console.log(countOccurrences('abcdefg', 'a'));
-console.log(countOccurrences('abcdefg', 'b'));
-console.log(countOccurrences('abccdefg', 'bcc'));
+console.log(reverseString('abcdefg'));
+
+// const reverseString = str => {
+//   let splitString = str.split('');
+//   let reverseArray = splitString.reverse();
+//   let joinArray = reverseArray.join('');
+//   return joinArray;
+// };
+
+// without verification
+// const reverseString = str => str.split('').reverse().join('');
