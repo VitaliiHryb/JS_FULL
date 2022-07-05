958b9e288555ef235a70b5af8b35045acdff7c8e
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-05 20:23:08 +0300

    Finding the min squared number

--- lesson-13/home-1/getMinSquaredNumber.js
+++ lesson-13/home-1/getMinSquaredNumber.js
@@ -17,9 +17,3 @@ export default array => {
 // console.log(getMinSquaredNumber([-777, 3, -2, 6, 45, -20]));
 
 // [-777, 3, -2, 6, 45, -20] => 4
-
-// Math.abs(x)
-
-// function difference(a, b) {
-//   return Math.abs(a - b);
-// }
