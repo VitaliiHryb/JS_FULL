1b14e998a0f77f914f737a1dbc11a51f9534b8f7
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-06 23:26:50 +0300

    memory calculator

--- lesson-14/home-1/index.js
+++ lesson-14/home-1/index.js
@@ -1,39 +1,21 @@
-/*
- * ф-ция getSenseOfLife не должна принимать аргументы
- * ф-ция getSenseOfLife должна вернуть число 42
- */
+// put your code here
+let acc = 0;
 
-// ... code here
+function add(number) {
+  acc += number;
+}
 
-/* ф-ция getSquared должна принимать число и вернуть квадрат этого числа */
+function decrease(number) {
+  acc -= number;
+}
 
-// ... code here
+function reset() {
+  acc = 0;
+}
 
-/* ф-ция sum должна принимать два числа и вернуть их сумму */
+function getMemo() {
+  return acc;
+}
 
-// ... code here
-
-/*
- * ф-ция sum должна принимать число
- * ф-ция sum должна вывести в консоль строку 'I am 34 years old'
- * где 34 - число, полученное в аргументах
- */
-
-// ... code here
-
-/* ======> Arrow functions <======= */
-
-/* ф-ция mult должна принимать два числа и вернуть их произведение */
-
-// ... code here
-
-/* ф-ция square должна принимать число и вернуть его, возведенное в квадрат */
-
-// ... code here
-
-/*
- * ф-ция getMagicNumber не должна принимать аргументы
- * ф-ция getMagicNumber должна вернуть число 17
- */
-
-// ... code here
+// export default let acc = 0; (dont understand how)
+export { add, decrease, reset, getMemo };
