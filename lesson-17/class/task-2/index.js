7447818cde39a092646032c5149812ffafdc7248
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-08 23:28:03 +0300

    callbackPrompt

--- lesson-17/class/task-2/index.js
+++ lesson-17/class/task-2/index.js
@@ -1,21 +1,53 @@
-// var головоломка
-/* eslint-disable */
+// Привязка контекста
+// callbackPrompt
 
-/* Начальный код для этой задачи написан с плохими практиками, такими как использования var, 
-переопределение переменной и тд. Это сделано намерено чтобы разобрать работу var.
-Чтобы eslint не ругался на ошибки, для этой задачи он отключен аннотацией eslint-disable */
+const callbackPrompt = {
+  message: 'Tell me your number',
+  showPrompt() {
+    const phoneNumber = prompt(this.message);
+    console.log(phoneNumber);
+  },
+  showDeferredPrompt(ms) {
+    // setTimeout(() => this.showPrompt(), ms);
+    // console.log(this);
+    setTimeout(this.showPrompt.bind(this), ms);
+  },
+};
 
-var foo = 1;
+export { callbackPrompt };
 
-function bar() {
-  if (!foo) {
-    let foo = 10;
-  }
+callbackPrompt.showDeferredPrompt(3000);
 
-  return foo;
-}
+// setTimeout(() => {console.log('run)}, 1000); // 1000 is ms
 
-var foo = bar();
+// explanation
+// const user = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   sayHi(age, message) {
+//     return `${message} ${this.firstName} and I'm ${age} years old`;
+//   },
+// };
 
-export default foo;
-// export default { foo };
+// // const func = user.getFullName;
+// // console.log(func());
+
+// // .bind();
+// // const func = user.getFullName.bind(user);
+// // console.log(func());
+
+// // .call();
+// const func = user.getFullName;
+
+// // console.log(func.call({ firstName: 'Tom', lastName: 'Jerry' }));
+
+// // .apply();
+// console.log(func.apply({ firstName: 'Tom', lastName: 'Jerry' }));
+
+// const func1 = user.sayHi;
+// const anotherUser = { firstName: 'Tom' };
+// console.log(func1.apply(anotherUser, [17, "Hi! I'm"]));
+// // if use method .call() ==> func1.apply(anotherUser, 17, "Hi! I'm");
