990cff96cd9afd7298a831c1066c0666a8faa2bc
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-08 15:52:39 +0300

    Function factory

--- lesson-16/home-1/index.js
+++ lesson-16/home-1/index.js
@@ -1,74 +1,36 @@
-'use strict';
+// Function factory
+/* eslint-disable */
 
-/**
- * @return {object}
- */
+/* Начальный код для этой задачи написан с плохими практиками, такими как использования var, 
+переопределение переменной и тд. Это сделано намерено чтобы разобрать работу var.
+Чтобы eslint не ругался на ошибки, для этой он отключен аннотацией eslint-disable */
 
-// createLogger
-// input: -
-// output: object (of methods)
+// input: number
+// output: [functions];
 
-// createMessage
-// input: (string, string) - message and type
-// output: object (message, dateTime, type)
+// createArrayOfFunctions(9)[5](); ==> 5
 
-// warn/error/log
-// input: (string) - message
-// output: -
+// if input === NaN return null;
+// if input === () return [];
 
-// getRecords
-// input: (string) - type
-// output: array
+// callback function = takes ()
+// callback function = return index
 
-// memory === array
-// type => 'warn', 'error' , 'log'
-// format: { message: *message text*, dateTime: *time message was sent*, type: *message type* }
-// dateTime <= use new Date() === current time
-// getRecords(type) return [{ message: *message text*, dateTime: *time message was sent*, type: *message type* }, ...]
-// if getRecords() => return messagers all type, else return message with needet type
-// sort by data Time from big to small
+const createArrayOfFunctions = len => {
+  if (len === undefined) return [];
+  if (typeof len !== 'number') return null;
 
-const createLogger = () => {
-  const messages = [];
+  const arr = [];
 
-  function createMessage(messageText, messageType) {
-    return {
-      message: messageText,
-      dateTime: new Date(),
-      type: messageType,
+  for (let index = 0; index < len; index += 1) {
+    arr[index] = function () {
+      return index;
     };
   }
 
-  function warn(messageText) {
-    const message = createMessage(messageText, 'warn');
-    messages.push(message);
-  }
-
-  function error(messageText) {
-    const message = createMessage(messageText, 'error');
-    messages.push(message);
-  }
-
-  function log(messageText) {
-    const message = createMessage(messageText, 'log');
-    messages.push(message);
-  }
-
-  function getRecords(type) {
-    let records;
-    if (type) {
-      records = messages.filter(message => message.type === type);
-    } else records = [...messages];
-
-    return records.sort((messege1, messege2) => messege2.dateTime - messege1.dateTime);
-  }
-
-  return {
-    warn,
-    error,
-    log,
-    getRecords,
-  };
+  return arr;
 };
 
-export { createLogger };
+// console.log(createArrayOfFunctions(9)[5]()); // ==> 5
+
+// export { createArrayOfFunctions };
