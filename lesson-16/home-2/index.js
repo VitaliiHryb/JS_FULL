4b017fb0cbe051b9e9e2560be06e8fc3cb8fa912
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-08 21:39:01 +0300

    hoisting

--- lesson-16/home-2/index.js
+++ lesson-16/home-2/index.js
@@ -1,74 +1,21 @@
-'use strict';
+// var головоломка
+/* eslint-disable */
 
-/**
- * @return {object}
- */
+/* Начальный код для этой задачи написан с плохими практиками, такими как использования var, 
+переопределение переменной и тд. Это сделано намерено чтобы разобрать работу var.
+Чтобы eslint не ругался на ошибки, для этой задачи он отключен аннотацией eslint-disable */
 
-// createLogger
-// input: -
-// output: object (of methods)
+var foo = 1;
 
-// createMessage
-// input: (string, string) - message and type
-// output: object (message, dateTime, type)
-
-// warn/error/log
-// input: (string) - message
-// output: -
-
-// getRecords
-// input: (string) - type
-// output: array
-
-// memory === array
-// type => 'warn', 'error' , 'log'
-// format: { message: *message text*, dateTime: *time message was sent*, type: *message type* }
-// dateTime <= use new Date() === current time
-// getRecords(type) return [{ message: *message text*, dateTime: *time message was sent*, type: *message type* }, ...]
-// if getRecords() => return messagers all type, else return message with needet type
-// sort by data Time from big to small
-
-const createLogger = () => {
-  const messages = [];
-
-  function createMessage(messageText, messageType) {
-    return {
-      message: messageText,
-      dateTime: new Date(),
-      type: messageType,
-    };
+function bar() {
+  if (!foo) {
+    let foo = 10;
   }
 
-  function warn(messageText) {
-    const message = createMessage(messageText, 'warn');
-    messages.push(message);
-  }
+  return foo;
+}
 
-  function error(messageText) {
-    const message = createMessage(messageText, 'error');
-    messages.push(message);
-  }
+var foo = bar();
 
-  function log(messageText) {
-    const message = createMessage(messageText, 'log');
-    messages.push(message);
-  }
-
-  function getRecords(type) {
-    let records;
-    if (type) {
-      records = messages.filter(message => message.type === type);
-    } else records = [...messages];
-
-    return records.sort((messege1, messege2) => messege2.dateTime - messege1.dateTime);
-  }
-
-  return {
-    warn,
-    error,
-    log,
-    getRecords,
-  };
-};
-
-export { createLogger };
+export default foo;
+// export default { foo };
