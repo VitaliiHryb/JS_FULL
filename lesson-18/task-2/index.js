bfecc86186308d42cc9c5aa864026680e68b5b2b
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-11 12:39:24 +0300

    wallet

--- lesson-18/task-2/index.js
+++ lesson-18/task-2/index.js
@@ -1,69 +1,26 @@
-/* ===> 1 <=== */
-const student = {
-  name: 'Tom',
-};
+'use strict';
+// заимствование методов
+// export { wallet }
 
-function sayName() {
-  console.log(this.name);
-}
-
-// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента
-sayName.apply(student);
-
-// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
-sayName.apply({ name: 'Bruce' });
-
-/* ===> 2 <=== */
-const company = {
-  companyName: 'Microsoft',
-};
-
-function greeting(firstName, lastName) {
-  console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
-}
-
-// вызовите ф-цию greeting так, чтобы в консоль вывелось
-// 'Hello, Bob Marley. Welcome to the Microsoft'
-// используйте объект company
-greeting.apply(company, ['Bob', 'Marley']);
-
-/* ===> 3 <=== */
-const country = {
-  countryName: 'Ukraine',
-  capital: 'Kyiv',
-};
-
-function getPopulation(population) {
-  return `Population in ${this.countryName} is ${population}`;
-}
-
-// вызовите ф-цию getPopulation так, чтобы она вернула
-// 'Population in Ukraine is 43000'
-// 43000 передавайте в виде числа
-// используйте объект country
-// результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
-const result = getPopulation.apply(country, [43000]);
-
-/* ===> 4 <=== */
-const transaction = {
-  amount: 1200,
-  operation: 'sell',
-  currency: 'USD',
-  exchange: 'NYSE',
-  printTransaction() {
-    console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
+const wallet = {
+  transactions: [1, 5, 89, 337, 3],
+  getMax() {
+    return Math.max(...this.transactions);
+  },
+  getMin() {
+    return Math.min(...this.transactions);
   },
 };
 
-const anotherTransaction = {
-  amount: 400,
-  operation: 'buy',
-  currency: 'USD',
-  exchange: 'NASDAQ',
-};
+console.log(wallet.getMax());
+console.log(wallet.getMin());
 
-// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
-// '400 USD - buy on NASDAQ'
-// используйте объект anotherTransaction как контекст
-console.log(result);
-transaction.printTransaction.apply(anotherTransaction);
+// const wallet = {
+//   transactions: [1, 5, 89, 337, 3],
+//   getMax() {
+//     return Math.max.apply(null, this.transactions);
+//   },
+//   getMin() {
+//     return Math.min.apply(null, this.transactions);
+//   },
+// };
