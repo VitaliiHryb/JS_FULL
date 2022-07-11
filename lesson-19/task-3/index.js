d26a3c4985a4db437ea4b66c53678856faa2d6b0
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-11 20:25:25 +0300

    constructor

--- lesson-19/task-3/index.js
+++ lesson-19/task-3/index.js
@@ -1,55 +1,68 @@
 'use strict';
+// Конструктор объектов - прототипы
 
-/**
- * @param {function} func
- * @return {function}
- */
+// export { User };
 
-// export { saveFuncCalls };
-
-function saveFuncCalls(func) {
-  withMemory.callsHistory = [];
-  function withMemory(...args) {
-    withMemory.callsHistory.push(args);
-    return func.apply(this, args);
-  }
-
-  return withMemory;
+function User(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-// example 1
-function sum(firstNum, secondNum) {
-  return firstNum + secondNum;
-}
-
-const sumWithMemory = saveFuncCalls(sum);
-sumWithMemory(4, 2); // ===> 6
-sumWithMemory(9, 1); // ===> 10
-
-sumWithMemory.callsHistory; // ===> [ [4, 2], [9, 1] ]
-
-// example 2
-function addDelta(array, delta) {
-  return array.map(el => el + delta);
-}
-
-const addDeltaWithMemory = saveFuncCalls(addDelta);
-addDeltaWithMemory([111, 22, 55, 4], 8); // ===> [119, 30, 63, 12]
-addDeltaWithMemory([9, 1, -8, 15, 7, 0], 7); // ===> [16, 8, -1, 22, 14, 7]
-
-addDeltaWithMemory.callsHistory; // ===> [ [[111, 22, 55, 4], 8], [[9, 1, -8, 15, 7, 0], 7] ]
-
-// example 3
-const user = {
-  name: 'John',
-  sayHi() {
-    return this.name;
-  },
+User.prototype.sayHi = function () {
+  console.log(`Hi, I am ${this.name}`);
 };
 
-const sayHiWithMemory = saveFuncCalls(user.sayHi);
-// sayHiWithMemory(); // ===> throw error // because sayHiWithMemory lost context
-const sayHiWithMemoryBinded = sayHiWithMemory.bind({ name: 'Tom' });
-console.log(sayHiWithMemoryBinded()); // ===> Tom // because we fixed context with bind and run functon again
+User.prototype.requestNewPhoto = function () {
+  console.log(`New photo request was sent for ${this.name}`);
+};
 
-console.log(sayHiWithMemory.callsHistory); // [ [] ]
+User.prototype.setAge = function (age) {
+  if (age < 0) return false;
+  this.age = age;
+  if (age >= 25) {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  return age;
+};
+
+// -----------------------------------------------------------
+// function User(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// User.prototype.sayHi = function () {
+//   console.log(`Hi, I am ${this.name}`);
+// };
+
+// User.prototype.requestNewPhoto = function () {
+//   console.log(`New photo request was sent for ${this.name}`);
+// };
+
+// User.prototype.setAge = function () {
+//   if (this.age < 0) return false;
+//   if (this.age >= 25) {
+//     console.log(`New photo request was sent for ${this.name}`);
+//   }
+//   return this.age;
+// };
+
+// ------------------------------------------------------------
+// const user1 = new User('Tom Soer', 17);
+// console.log(user1);
+
+// user1.sayHi();
+
+// const user2 = new User('Jordan Piterson', 57);
+// user2.sayHi();
+
+// console.log(user2.sayHi === user1.sayHi);
+
+const user3 = new User('John', 57);
+user3.setAge();
+
+const user4 = new User('Ann', 17);
+user4.setAge();
+
+const user5 = new User('Ann', -17);
+console.log(user5.setAge());
