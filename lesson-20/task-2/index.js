7edf72216bb4ae7e1e24fb4ebff24c5e52d91a2e
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-12 15:06:59 +0300

    JS L20

---
+++ lesson-20/task-2/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  },
+};
+
+export default user;
