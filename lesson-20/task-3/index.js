58ed8105ba1c28dcb3dc21a62acfc162059ba1a3
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-12 17:58:12 +0300

    Wallet

--- lesson-20/task-3/index.js
+++ lesson-20/task-3/index.js
@@ -1,16 +1,37 @@
 'use strict';
 
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-  set fullName(value) {
-    const [firstName, lastName] = value.split(' ');
-    this.firstName = firstName;
-    this.lastName = lastName;
-  },
-};
+// Wallet
+// export { Wallet };
 
-export default user;
+class Wallet {
+  #balance = 0;
+
+  getBalance() {
+    return this.#balance;
+  }
+
+  deposit(amount) {
+    this.#balance += amount;
+  }
+
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      console.log('No enough funds');
+      return;
+    }
+
+    this.#balance -= amount;
+  }
+}
+
+const wallet1 = new Wallet();
+
+console.log(wallet1.getBalance());
+wallet1.deposit(45);
+console.log(wallet1.getBalance());
+wallet1.withdraw(34);
+console.log(wallet1.getBalance());
+wallet1.withdraw(134);
+console.log(wallet1.getBalance());
+
+// console.log(wallet1.#balance());
