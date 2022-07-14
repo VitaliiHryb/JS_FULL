a0b83724508940a50debba3094e2ec030a6c4b65
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-14 15:42:12 +0300

    Finding multiple elements on a page

--- lesson-21/home-2/index.js
+++ lesson-21/home-2/index.js
@@ -1,86 +1,20 @@
 /* eslint-disable max-classes-per-file */
+// Finding multiple elements on a page
 
-export class User {
-  constructor(id, name, sessionId) {
-    this._id = id;
-    this._name = name;
-    this._sessionId = sessionId;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get sessionId() {
-    return this._sessionId;
-  }
+export function getItemsList() {
+  const elementList = document.querySelectorAll('.technology');
+  console.dir(elementList);
+  return elementList;
 }
 
-export class UserRepository {
-  constructor(users) {
-    this._users = Object.freeze(users);
-  }
+// NodeList ==> Array.from
 
-  get users() {
-    this._users;
-  }
-
-  getUserNames() {
-    return this.users.map(({ name }) => name);
-  }
-
-  getUserIds() {
-    return this.users.map(({ id }) => id);
-  }
-
-  getUserNameById(userId) {
-    const user = this.users.find(({ id }) => id === userId);
-    return user ? user.name : null;
-  }
+export function getItemsArray() {
+  const elementArray = Array.from(document.querySelectorAll('.tool'));
+  // list ==> array
+  console.dir(elementArray);
+  return elementArray;
 }
-// class User {
-//   constructor(id, name, sessionId) {
-//     this._id = id;
-//     this._name = name;
-//     this._sessionId = sessionId;
-//   }
-// }
 
-// class UserRepository {
-//   constructor(users) {
-//     this._users = Object.freeze(users);
-//   }
-
-//   getUserNames() {
-//     return this._users.map(({ _name }) => _name);
-//   }
-
-//   getUserIds() {
-//     return this._users.map(({ _id }) => _id);
-//   }
-
-//   getUserNameById(userId) {
-//     const user = this._users.find(({ _id }) => _id === userId);
-//     return user ? user._name : null;
-//   }
-// }
-
-// export { User, UserRepository };
-
-// examples
-const user = new User('1', 'Tom', 'session-id');
-
-// получить свойства можем
-console.log(user.id); // ===> '1'
-console.log(user.name); // ===> 'Tom'
-console.log(user.sessionId); // ===> 'session-id'
-
-// но изменить эти свойства нельзя
-// user.name = 'Bob'; // пытаемся изменить старое значение
-// console.log(user.name); // ===> 'Tom' - но изменение проигнорировано, так как setter отсутствует
-user.name = 'Bob';
-console.log(user.name);
+// getItemsList();
+// getItemsArray();
