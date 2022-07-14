7cf54beff2248f77a04975248e3a4d058dc10d74
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-14 15:19:46 +0300

    Finding an element on a page

--- lesson-21/home-1/index.js
+++ lesson-21/home-1/index.js
@@ -1,50 +1,17 @@
 'use strict';
+// Finding an element on a page
 
-/**
- * @field {string} id
- * @field {number} price
- * @field {date} dateCreated
- * @field {boolean} isConfirmed
- * @field {date} dateConfirmed
- * @field {string} city
- * @field {string} type
- */
-export class Order {
-  constructor(price, city, type) {
-    this.id = `${Math.random()}`;
-    this.price = price;
-    this.city = city;
-    this.type = type;
-
-    this.dateCreated = new Date();
-    this.isConfirmed = false;
-    this.dateConfirmed = null;
-  }
-
-  checkPrice() {
-    if (this.price > 100) {
-      return true;
-    }
-    return false;
-  }
-
-  confirmOrder() {
-    this.isConfirmed = true;
-    this.dateConfirmed = new Date();
-  }
-
-  isValidType(Buy, Sell) {
-    if (this.type === 'Buy' || this.type === 'Sell') {
-      return true;
-    }
-    return false;
-  }
+export function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
 }
 
-// console.log(wallet1.getBalance());
-// wallet1.deposit(45);
-// console.log(wallet1.getBalance());
-// wallet1.withdraw(34);
-// console.log(wallet1.getBalance());
-// wallet1.withdraw(134);
-// console.log(wallet1.getBalance());
+export function getInputElement() {
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem);
+  return inputElem;
+}
+
+// getTitleElement();
+// getInputElement();
