ef41337e56066e1071e62e5e63be0234d614a93d
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-15 00:06:23 +0300

    Setting attributes for DOM elements

--- lesson-21/home-9/index.js
+++ lesson-21/home-9/index.js
@@ -1,74 +1,22 @@
-// 'use strict';
-{
-    "max-classes-per-file": ["error", 1]
-}
-/* eslint max-classes-per-file: ["error", { ignoreExpressions: true }] */
+'use strict';
+// Установка атрибутов для DOM элементов
 
+// paste in input ==> name="login"
+// finishForm must includes <type> with text on password for input with parol
 
-// ------------------------------ photo request was sent -----------------------------------
-// // static compare(user1, user2) {
-// //   return user1.age - user2.age;
-// // }
-// // const user1 = new User('Tom', 17);
-// // const user2 = new User('Tom', 37);
-// // console.log(User.compare(user1, user2));
-
-class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  sayHi() {
-    console.log(`Hi, I am ${this.name}`);
-  }
-
-  requestNewPhoto() {
-    console.log(`New photo request was sent for ${this.name}`);
-  }
-
-  setAge(value) {
-    if (value < 0) {
-      return false;
-    }
-    this.age = value;
-    if (value >= 25) {
-      this.requestNewPhoto();
-    }
-    return value;
-  }
-
-  static createEmpty() {
-    return new User('', null);
-  }
+function finishForm() {
+  const target = document.querySelector('form');
+  const formItem = document.createElement('input');
+  target.prepend(formItem);
+  formItem.type = 'text';
+  formItem.name = 'login';
+  return formItem;
 }
 
-// export { User };
+// finishForm();
 
-// const user1 = new User('Tom', 17);
-// console.log(user1);
+export { finishForm };
 
-// // // конструктор в функции
-// // function User(name,age) {
-// //   this.name = name;
-// //   this.age = age,
-// // }
-
-// // User.prototype.sayHi = function() {
-// //   console.log(`Hi, I am ${this.name}`);
-// // }
-
-// // User.prototype.requestNewPhoto = function() {
-// //   console.log(`New photo request was sent for ${this.name}`);
-// // }
-
-// // User.prototype.setAge = function(value) {
-// //   if (value < 0) {
-// //     return false;
-// //   }
-// //   this.age = value;
-// //   if (value >= 25) {
-// //     this.requestNewPhoto();
-// //   }
-// //   return value;
-// // }
+// add type and name
+// inp2.type = 'text';
+// inp2.name = 'input_2';
