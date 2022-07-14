cbdcfa524616ff75e3750ad18715d1486bc39353
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-14 16:05:50 +0300

    Getting the content of an element

--- lesson-21/home-3/index.js
+++ lesson-21/home-3/index.js
@@ -1,74 +1,24 @@
-// 'use strict';
-{
-    "max-classes-per-file": ["error", 1]
-}
-/* eslint max-classes-per-file: ["error", { ignoreExpressions: true }] */
+'use strict';
+// Получение содержимого элемента
 
-
-// ------------------------------ photo request was sent -----------------------------------
-// // static compare(user1, user2) {
-// //   return user1.age - user2.age;
-// // }
-// // const user1 = new User('Tom', 17);
-// // const user2 = new User('Tom', 37);
-// // console.log(User.compare(user1, user2));
-
-class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  sayHi() {
-    console.log(`Hi, I am ${this.name}`);
-  }
-
-  requestNewPhoto() {
-    console.log(`New photo request was sent for ${this.name}`);
-  }
-
-  setAge(value) {
-    if (value < 0) {
-      return false;
-    }
-    this.age = value;
-    if (value >= 25) {
-      this.requestNewPhoto();
-    }
-    return value;
-  }
-
-  static createEmpty() {
-    return new User('', null);
-  }
+function getTitle() {
+  const textContent = document.querySelector('.title');
+  return textContent;
 }
 
-// export { User };
+function getDescription() {
+  const textContent = document.querySelector('.about');
+  return textContent.innerText;
+}
 
-// const user1 = new User('Tom', 17);
-// console.log(user1);
+function getPlans() {
+  const textContent = document.querySelector('.plans');
+  return textContent.innerHTML;
+}
 
-// // // конструктор в функции
-// // function User(name,age) {
-// //   this.name = name;
-// //   this.age = age,
-// // }
+function getGoal() {
+  const textContent = document.querySelector('.goal');
+  return textContent.outerHTML;
+}
 
-// // User.prototype.sayHi = function() {
-// //   console.log(`Hi, I am ${this.name}`);
-// // }
-
-// // User.prototype.requestNewPhoto = function() {
-// //   console.log(`New photo request was sent for ${this.name}`);
-// // }
-
-// // User.prototype.setAge = function(value) {
-// //   if (value < 0) {
-// //     return false;
-// //   }
-// //   this.age = value;
-// //   if (value >= 25) {
-// //     this.requestNewPhoto();
-// //   }
-// //   return value;
-// // }
+export { getTitle, getDescription, getPlans, getGoal };
