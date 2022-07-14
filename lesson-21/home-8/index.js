3f4a0d02c4ede45fe0060b3f395bc3e23876d36e
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-14 23:07:17 +0300

    insert per page - 2

--- lesson-21/home-8/index.js
+++ lesson-21/home-8/index.js
@@ -1,74 +1,39 @@
-// 'use strict';
-{
-    "max-classes-per-file": ["error", 1]
-}
-/* eslint max-classes-per-file: ["error", { ignoreExpressions: true }] */
+'use strict';
+// Создание DOM элементов и вставка на страницу - 2
 
+// use append, prepend, before, after
+// output list from 1 to 8
 
-// ------------------------------ photo request was sent -----------------------------------
-// // static compare(user1, user2) {
-// //   return user1.age - user2.age;
-// // }
-// // const user1 = new User('Tom', 17);
-// // const user2 = new User('Tom', 37);
-// // console.log(User.compare(user1, user2));
-
-class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  sayHi() {
-    console.log(`Hi, I am ${this.name}`);
-  }
-
-  requestNewPhoto() {
-    console.log(`New photo request was sent for ${this.name}`);
-  }
-
-  setAge(value) {
-    if (value < 0) {
-      return false;
-    }
-    this.age = value;
-    if (value >= 25) {
-      this.requestNewPhoto();
-    }
-    return value;
-  }
-
-  static createEmpty() {
-    return new User('', null);
-  }
+function finishList() {
+  const target1 = document.querySelector('.list');
+  const target2 = document.querySelector('.special');
+  const liItem1 = document.createElement('li');
+  const liItem4 = document.createElement('li');
+  const liItem6 = document.createElement('li');
+  const liItem8 = document.createElement('li');
+  target1.prepend(liItem1);
+  target2.before(liItem4);
+  target2.after(liItem6);
+  target1.append(liItem8);
+  liItem1.textContent = '1';
+  liItem4.textContent = '4';
+  liItem6.textContent = '6';
+  liItem8.textContent = '8';
 }
 
-// export { User };
+// finishList();
 
-// const user1 = new User('Tom', 17);
-// console.log(user1);
+export { finishList };
 
-// // // конструктор в функции
-// // function User(name,age) {
-// //   this.name = name;
-// //   this.age = age,
-// // }
-
-// // User.prototype.sayHi = function() {
-// //   console.log(`Hi, I am ${this.name}`);
-// // }
-
-// // User.prototype.requestNewPhoto = function() {
-// //   console.log(`New photo request was sent for ${this.name}`);
-// // }
-
-// // User.prototype.setAge = function(value) {
-// //   if (value < 0) {
-// //     return false;
-// //   }
-// //   this.age = value;
-// //   if (value >= 25) {
-// //     this.requestNewPhoto();
-// //   }
-// //   return value;
-// // }
+/*
+<ul class="list"> <!-- target1 -->
+  <!-- prepend -->
+  <li>2</li>
+  <li>3</li>
+  <!-- before -->
+  <li class="special">5</li> <!-- target2 -->
+  <!-- after -->
+  <li>7</li>
+  <!-- append -->
+</ul>
+*/
