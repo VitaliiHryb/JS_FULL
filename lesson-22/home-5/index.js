01d23e743c06cca1aca9e48551c9bb7764f423c2
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-15 20:15:28 +0300

    Single 'click'

--- lesson-22/home-5/index.js
+++ lesson-22/home-5/index.js
@@ -1,29 +1,14 @@
 'use strict';
-// Установка дочерних элементов
+// Single обработка события 'click' на кнопку
+// use addEventListener
 
-// const some result = document.querySelector('.title');
-
-function setButton(buttonText) {
-  const result = document.querySelector('body');
-  result.innerHTML = `<button>${buttonText}</button>`;
-  return result;
+const singleUseButt = document.querySelector('.single-use-btn');
+function handleClick() {
+  // alert(`${taskStatus.value}`);
+  console.log(`clicked`);
+  singleUseButt.removeEventListener('click', handleClick);
 }
+singleUseButt.addEventListener('click', handleClick);
+// singleUseButt.removeEventListener('click', handleClick);
 
-// setButton('button text');
-
-// function setButton(buttonText) {
-//   const result = document.querySelector('body');
-//   result.innerHTML = buttonText;
-//   return result;
-// }
-
-// function setButton2(buttonText) {
-//   const result = document.querySelector('body');
-//   result.textContent = buttonText;
-//   return result;
-// }
-
-// setButton('<button>button text</button>');
-// setButton2('<button>button text</button>');
-
-export { setButton };
+// export {}
