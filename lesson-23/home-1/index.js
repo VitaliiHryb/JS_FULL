4d63674e86c031623117da5b4f72d26065719a10
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-18 23:47:18 +0300

    Todo List

--- lesson-23/home-1/index.js
+++ lesson-23/home-1/index.js
@@ -15,96 +15,173 @@
 // 4. keep in mind that data-attribute will have data - 'string'
 
 // Algo:
-// 1. create button for creating tasks
-// 1a. function(id, text, isDone)
-// 2. createElement(elementType, class_, attrs, dataset)
-// 3. element(task) shoud have elementType, class, attrs, dataset
-
-// option1
-const listElem = document.querySelector('.list');
-const createTaskBtnElem = document.querySelector('.create-task-btn');
+// 1. create action for button ( ==> creating tasks )
+// 1a. onclick use renderTask
+// use getInput
 
 const tasks = [
-  { id: 1, text: 'Buy milk', isDone: false },
-  { id: 2, text: 'Pick up Tom from airport', isDone: false },
-  { id: 3, text: 'Visit party', isDone: false },
-  { id: 4, text: 'Visit doctor', isDone: true },
-  { id: 5, text: 'Buy meat', isDone: true },
+  { id: '1', text: 'Buy milk', done: false },
+  { id: '2', text: 'Pick up Tom from airport', done: false },
+  { id: '3', text: 'Visit party', done: false },
+  { id: '4', text: 'Visit doctor', done: true },
+  { id: '5', text: 'Buy meat', done: true },
 ];
 
-function createTask(id, text, isDone = false) {
-  return { id, text, isDone };
-}
+// for click
+const createButton = document.querySelector('button');
+const taskInput = document.querySelector('.task-input');
 
-// what is attrs? It is an array of keys ==> add new attribute (name: value)
-function createElement(elementType, class_, attrs, dataset) {
-  const element = document.createElement(elementType);
-
-  if (class_) element.classList.add(class_);
-
-  if (attrs)
-    Object.keys(attrs).forEach(attr => {
-      element.setAttribute(attr, attrs[attr]);
-    });
-
-  if (dataset)
-    Object.keys(dataset).forEach(dataKey => {
-      element.dataset[dataKey] = dataset[dataKey];
-    });
-
-  return element;
-}
-
-function renderDecorator(callbackFn) {
-  return function () {
-    const fnResult = callbackFn.apply(this, arguments);
-
-    listElem.innerHTML = '';
-    renderTasks(tasks);
-
-    return fnResult;
-  };
-}
-
-const onCheckboxClick = renderDecorator(({ target }) => {
-  const taskId = +target.closest('.list__item').dataset.taskId;
-  const task = tasks.find(task => task.id === taskId);
-  task.isDone = !task.isDone;
-  console.log(tasks, task);
-});
-
-const onCreateBtnClick = renderDecorator(() => {
-  const newTaskElement = document.querySelector('.task-input');
-  if (!newTaskElement.value) return;
-
-  const id = Math.max(...tasks.map(task => task.id)) + 1;
-  tasks.push(createTask(id, newTaskElement.value));
-  newTaskElement.value = '';
-});
-
-createTaskBtnElem.addEventListener('click', onCreateBtnClick);
+const listElem = document.querySelector('.list');
 
 const renderTasks = tasksList => {
+  removeListeners();
+
+  while (listElem.firstChild) {
+    listElem.removeChild(listElem.firstChild);
+  }
+
   const tasksElems = tasksList
-    .sort((a, b) => a.isDone - b.isDone)
-    .map(({ id, text, isDone }) => {
-      const listItemElem = createElement('li', 'list__item', null, {
-        taskId: id,
-      });
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
 
-      const checkbox = createElement('input', 'list__item-checkbox', {
-        type: 'checkbox',
-      });
-      checkbox.checked = isDone;
-      checkbox.addEventListener('click', onCheckboxClick);
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.dataset.id = id;
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
 
-      if (isDone) listItemElem.classList.add('list__item_done');
-
+      checkbox.addEventListener('change', changeTaskState);
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
       listItemElem.append(checkbox, text);
+      // <li class="list__item list__item_done">
+      //   <input type="checkbox" class="list__item-checkbox">
+      //   Visit
+      // </li>
+
       return listItemElem;
     });
 
+  // push new task in list
   listElem.append(...tasksElems);
 };
 
-renderTasks(tasks); // render test data by default
+function changeTaskState(event) {
+  const checkbox = event.target;
+  const { id } = checkbox.dataset;
+  const task = tasks.find(t => t.id === id);
+  task.done = checkbox.checked;
+  renderTasks(tasks);
+}
+
+function removeListeners() {
+  const checkboxes = document.querySelectorAll('.list__item-checkbox');
+  checkboxes.forEach(c => c.removeEventListener('change', changeTaskState));
+}
+
+function createNewTask() {
+  const textInInput = taskInput.value;
+  const newId = Math.max(...tasks.map(t => +t.id)) + 1;
+
+  const addedTask = { text: textInInput, done: false, id: newId.toString() };
+  tasks.push(addedTask);
+
+  renderTasks(tasks);
+}
+createButton.addEventListener('click', createNewTask);
+
+renderTasks(tasks);
+
+// -----------------------------------------------------------------------------------
+
+// // perfect solution
+// const listElem = document.querySelector('.list');
+// const createTaskBtnElem = document.querySelector('.create-task-btn');
+
+// const tasks = [
+//   { id: 1, text: 'Buy milk', isDone: false },
+//   { id: 2, text: 'Pick up Tom from airport', isDone: false },
+//   { id: 3, text: 'Visit party', isDone: false },
+//   { id: 4, text: 'Visit doctor', isDone: true },
+//   { id: 5, text: 'Buy meat', isDone: true },
+// ];
+
+// function createTask(id, text, isDone = false) {
+//   return { id, text, isDone };
+// }
+
+// // what is attrs? It is an array of keys ==> add new attribute (name: value)
+// function createElement(elementType, class_, attrs, dataset) {
+//   const element = document.createElement(elementType);
+
+//   if (class_) element.classList.add(class_);
+
+//   if (attrs)
+//     Object.keys(attrs).forEach(attr => {
+//       element.setAttribute(attr, attrs[attr]);
+//     });
+
+//   if (dataset)
+//     Object.keys(dataset).forEach(dataKey => {
+//       element.dataset[dataKey] = dataset[dataKey];
+//     });
+
+//   return element;
+// }
+
+// function renderDecorator(callbackFn) {
+//   return function () {
+//     const fnResult = callbackFn.apply(this, arguments);
+
+//     listElem.innerHTML = '';
+//     renderTasks(tasks);
+
+//     return fnResult;
+//   };
+// }
+
+// const onCheckboxClick = renderDecorator(({ target }) => {
+//   const taskId = +target.closest('.list__item').dataset.taskId;
+//   const task = tasks.find(task => task.id === taskId);
+//   task.isDone = !task.isDone;
+//   console.log(tasks, task);
+// });
+
+// const onCreateBtnClick = renderDecorator(() => {
+//   const newTaskElement = document.querySelector('.task-input');
+//   if (!newTaskElement.value) return;
+
+//   const id = Math.max(...tasks.map(task => task.id)) + 1;
+//   tasks.push(createTask(id, newTaskElement.value));
+//   newTaskElement.value = '';
+// });
+
+// createTaskBtnElem.addEventListener('click', onCreateBtnClick);
+
+// const renderTasks = tasksList => {
+//   const tasksElems = tasksList
+//     .sort((a, b) => a.isDone - b.isDone)
+//     .map(({ id, text, isDone }) => {
+//       const listItemElem = createElement('li', 'list__item', null, {
+//         taskId: id,
+//       });
+
+//       const checkbox = createElement('input', 'list__item-checkbox', {
+//         type: 'checkbox',
+//       });
+//       checkbox.checked = isDone;
+//       checkbox.addEventListener('click', onCheckboxClick);
+
+//       if (isDone) listItemElem.classList.add('list__item_done');
+
+//       listItemElem.append(checkbox, text);
+//       return listItemElem;
+//     });
+
+//   listElem.append(...tasksElems);
+// };
+
+// renderTasks(tasks); // render test data by default
