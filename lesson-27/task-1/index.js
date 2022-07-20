fec1853e019455b5e93db791f5bfa185d0824f26
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-20 15:33:50 +0300

    getLocalStorageData

--- lesson-27/task-1/index.js
+++ lesson-27/task-1/index.js
@@ -1,39 +1,74 @@
-/*
- * ф-ция getSenseOfLife не должна принимать аргументы
- * ф-ция getSenseOfLife должна вернуть число 42
- */
+// Получение всех данных localStorage
 
-// ... code here
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
 
-/* ф-ция getSquared должна принимать число и вернуть квадрат этого числа */
+export { getLocalStorageData };
 
-// ... code here
+// localStorage.clear();
+// // localStorage.setItem('hobbies', [1, 2, 3, 4]);
+// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+// // localStorage.setItem('name', JSON.stringify('Tom'));
+// localStorage.setItem('name', 'Tom');
+// localStorage.setItem('age', JSON.stringify(17));
 
-/* ф-ция sum должна принимать два числа и вернуть их сумму */
+// // console.log(JSON.parse(localStorage.getItem('hobbies')));
 
-// ... code here
+// const getLocalStorageData = () => {
+//   // option 1
+//   // for (let i = 0; i < localStorage.length; i += 1) {
+//   //   localStorage.key(i);
+//   // }
+//   // option 2
+//   // for (let i of localStorage) {
+//   //   localStorage.key(i);
+//   // }
+//   // option 3
+//   // Object.keys(localStorage);
+//   // Object.values(localStorage);
+//   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+//     let newValue;
+//     try {
+//       newValue = JSON.parse(value);
+//     } catch (e) {
+//       newValue = value;
+//     }
+//     return {
+//       ...acc,
+//       // [key]: JSON.parse(value),
+//       [key]: newValue,
+//     };
+//   }, {});
+// };
 
-/*
- * ф-ция sum должна принимать число
- * ф-ция sum должна вывести в консоль строку 'I am 34 years old'
- * где 34 - число, полученное в аргументах
- */
+// console.log(getLocalStorageData());
 
-// ... code here
+// localStorage.getItem('name');
 
-/* ======> Arrow functions <======= */
+// example
 
-/* ф-ция mult должна принимать два числа и вернуть их произведение */
-
-// ... code here
-
-/* ф-ция square должна принимать число и вернуть его, возведенное в квадрат */
-
-// ... code here
-
-/*
- * ф-ция getMagicNumber не должна принимать аргументы
- * ф-ция getMagicNumber должна вернуть число 17
- */
-
-// ... code here
+// const user = {
+//   name: 'Tom',
+//   age: 25,
+//   isStudent: false,
+//   driverLicense: null,
+//   hobbies: ['football', 'diving'],
+//   education: [
+//     {
+//       name: 'MIT Precourse',
+//       gradueteDate: '2020-05-04T14:48:46.105Z',
+//     },
+//   ],
+// };
