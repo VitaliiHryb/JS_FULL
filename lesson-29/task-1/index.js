ef6fe30f22cd3c28d604be6e054120bf9f65cbae
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-22 23:08:31 +0300

    Load image

--- lesson-29/task-1/index.js
+++ lesson-29/task-1/index.js
@@ -15,6 +15,16 @@ function addImage(imgSrc, callback) {
   imgElem.addEventListener('error', () => callback('Image load failed'));
 }
 
+function onImageLoaded(error, data) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+}
+
 export { addImage };
 
 // const addImage = (imgSrc, callback) => {
