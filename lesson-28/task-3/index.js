e566ea2a2ce155481d9d9537f9f7326f95b84297
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-22 14:33:28 +0300

    currying

--- lesson-28/task-3/index.js
+++ lesson-28/task-3/index.js
@@ -1,74 +1,22 @@
-// Получение всех данных localStorage
+// Каррирование
 
-const getLocalStorageData = () => {
-  return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    let newValue;
-    try {
-      newValue = JSON.parse(value);
-    } catch (e) {
-      newValue = value;
-    }
-    return {
-      ...acc,
-      [key]: newValue,
-    };
-  }, {});
-};
+// // const sum = (a, b) => a + b;
 
-export { getLocalStorageData };
+// const sum = a => b => a + b;
 
-// localStorage.clear();
-// // localStorage.setItem('hobbies', [1, 2, 3, 4]);
-// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-// // localStorage.setItem('name', JSON.stringify('Tom'));
-// localStorage.setItem('name', 'Tom');
-// localStorage.setItem('age', JSON.stringify(17));
+// const add3 = sum(3);
 
-// // console.log(JSON.parse(localStorage.getItem('hobbies')));
+// const result = add3(5);
+// // console.log(result);
 
-// const getLocalStorageData = () => {
-//   // option 1
-//   // for (let i = 0; i < localStorage.length; i += 1) {
-//   //   localStorage.key(i);
-//   // }
-//   // option 2
-//   // for (let i of localStorage) {
-//   //   localStorage.key(i);
-//   // }
-//   // option 3
-//   // Object.keys(localStorage);
-//   // Object.values(localStorage);
-//   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-//     let newValue;
-//     try {
-//       newValue = JSON.parse(value);
-//     } catch (e) {
-//       newValue = value;
-//     }
-//     return {
-//       ...acc,
-//       // [key]: JSON.parse(value),
-//       [key]: newValue,
-//     };
-//   }, {});
-// };
+const mult = a => b => a * b;
 
-// console.log(getLocalStorageData());
+const twice = mult(2);
+// console.log(twice(9));
 
-// localStorage.getItem('name');
+const triple = mult(3 / 2);
 
-// example
+// const result = triple(twice(9));
+// console.log(result);
 
-// const user = {
-//   name: 'Tom',
-//   age: 25,
-//   isStudent: false,
-//   driverLicense: null,
-//   hobbies: ['football', 'diving'],
-//   education: [
-//     {
-//       name: 'MIT Precourse',
-//       gradueteDate: '2020-05-04T14:48:46.105Z',
-//     },
-//   ],
-// };
+export { mult };
