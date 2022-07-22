baa6ecfc4ab76b2d5a94294d5475badc3e9448f0
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-22 13:34:24 +0300

    Traversing a tree with recursion

--- lesson-28/task-2/index.js
+++ lesson-28/task-2/index.js
@@ -1,74 +1,46 @@
-// Получение всех данных localStorage
+// рекурсия
+// Проход по дереву с помощью рекурсии
 
-const getLocalStorageData = () => {
-  return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    let newValue;
-    try {
-      newValue = JSON.parse(value);
-    } catch (e) {
-      newValue = value;
-    }
-    return {
-      ...acc,
-      [key]: newValue,
-    };
-  }, {});
+const favorites = ['id-6', 'id-17'];
+
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  // isFavorite: true, // add this
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [
+        {
+          id: 'id-6',
+          name: 'Drinks',
+          nodes: [],
+        },
+      ],
+    },
+    {
+      id: 'id-17',
+      name: 'Vehicles',
+      nodes: [],
+    },
+  ],
 };
 
-export { getLocalStorageData };
+// console.log(tree);
 
-// localStorage.clear();
-// // localStorage.setItem('hobbies', [1, 2, 3, 4]);
-// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-// // localStorage.setItem('name', JSON.stringify('Tom'));
-// localStorage.setItem('name', 'Tom');
-// localStorage.setItem('age', JSON.stringify(17));
+// markFavorites must add info isFavorite: true or false
 
-// // console.log(JSON.parse(localStorage.getItem('hobbies')));
+const markFavorites = (tree, favorites) => {
+  const isFavorite = favorites.includes(tree.id);
 
-// const getLocalStorageData = () => {
-//   // option 1
-//   // for (let i = 0; i < localStorage.length; i += 1) {
-//   //   localStorage.key(i);
-//   // }
-//   // option 2
-//   // for (let i of localStorage) {
-//   //   localStorage.key(i);
-//   // }
-//   // option 3
-//   // Object.keys(localStorage);
-//   // Object.values(localStorage);
-//   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-//     let newValue;
-//     try {
-//       newValue = JSON.parse(value);
-//     } catch (e) {
-//       newValue = value;
-//     }
-//     return {
-//       ...acc,
-//       // [key]: JSON.parse(value),
-//       [key]: newValue,
-//     };
-//   }, {});
-// };
+  return {
+    ...tree,
+    isFavorite,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
 
-// console.log(getLocalStorageData());
+console.log(markFavorites(tree, favorites));
 
-// localStorage.getItem('name');
-
-// example
-
-// const user = {
-//   name: 'Tom',
-//   age: 25,
-//   isStudent: false,
-//   driverLicense: null,
-//   hobbies: ['football', 'diving'],
-//   education: [
-//     {
-//       name: 'MIT Precourse',
-//       gradueteDate: '2020-05-04T14:48:46.105Z',
-//     },
-//   ],
-// };
+export { markFavorites };
