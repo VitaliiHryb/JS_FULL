805161350301a6df325c53e2ae676cb96241e249
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-26 23:25:57 +0300

    Almost a request to the server

--- lesson-31/home-2/index.js
+++ lesson-31/home-2/index.js
@@ -1,34 +1,36 @@
-// Успешные (resolved) промисы
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise(resolve => {
-  setTimeout(() => {
-    resolve({ name: 'Tom', age: 17 });
-  }, 1000);
+// Почти запрос на сервер
+const serverResponsePromise = new Promise(resolve => {
+  const serverResponse = {
+    ok: true,
+    json() {
+      return Promise.resolve({
+        name: 'John',
+        age: 20,
+      });
+    },
+  };
+  resolve(serverResponse);
 });
 
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ * допиши первый обработчик, чтобы во второй попал объект пользователя
  */
 
-console.log(userDataPromise);
-// console.log(typeof userDataPromise); // ==> object
+serverResponsePromise
+  .then(
+    response =>
+      new Promise(resolve => {
+        // console.log(response.json);
+        const jObj = response.json;
+        const result = jObj();
+        // console.log(jObj);
+        resolve(result);
+      }),
+  )
+  .then(result => {
+    console.log(result); // { name: 'John', age: 20 }
+  });
 
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
- */
-userDataPromise.then(function onSuccess(userData) {
-  console.log(userData); // ==> { name: 'Tom', age: 17 }
-});
-
-/*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
- */
-
-userDataPromise.then(function onSuccess(data) {
-  console.log(`My name is ${data.name}. I am ${data.age} years old`);
-});
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
