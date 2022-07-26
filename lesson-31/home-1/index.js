1268c6a9342bfbe2f105b848ff2678cfd8e0a996
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-26 22:36:41 +0300

    Promise chain fix

--- lesson-31/home-1/index.js
+++ lesson-31/home-1/index.js
@@ -1,34 +1,43 @@
-// Успешные (resolved) промисы
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise(resolve => {
-  setTimeout(() => {
-    resolve({ name: 'Tom', age: 17 });
-  }, 1000);
+// Фикс цепочки промисов
+const successPromise = new Promise(resolve => {
+  resolve(32);
 });
 
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
  */
 
-console.log(userDataPromise);
-// console.log(typeof userDataPromise); // ==> object
+successPromise
+  .then(number => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then(
+    number =>
+      new Promise(resolve => {
+        const squaredNumber = number * number;
+        resolve(squaredNumber);
+      }),
+  )
+  .then(result => {
+    console.log(result); // 256
+  });
 
 /*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
  */
-userDataPromise.then(function onSuccess(userData) {
-  console.log(userData); // ==> { name: 'Tom', age: 17 }
-});
+successPromise
+  .then(
+    number =>
+      new Promise(resolve => {
+        const tenNumber = number * 10;
+        resolve(tenNumber);
+      }),
+  )
+  .then(result => {
+    console.log(result); // 320
+  });
 
-/*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
- */
-
-userDataPromise.then(function onSuccess(data) {
-  console.log(`My name is ${data.name}. I am ${data.age} years old`);
-});
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
