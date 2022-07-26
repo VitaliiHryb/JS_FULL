a6966040680c2bae22f10a425815d403ad585b4d
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-26 18:27:07 +0300

    Promise handlers

--- lesson-31/task-1/index.js
+++ lesson-31/task-1/index.js
@@ -1,23 +1,60 @@
-let done = true;
+// Promise handlers & chaining
+// Promise handlers
+'use strict';
 
-const isItDoneYet = new Promise((resolve, reject) => {
-  if (done) {
-    const workDone = 'Here is the thing I built';
-    resolve(workDone);
-  } else {
-    const why = 'Still working on something else';
-    reject(why);
-  }
-});
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: `${userId}`,
+        });
+      }, 1000);
+    }
+  });
 
-const checkIfItsDone = () => {
-  isItDoneYet
-    .then(ok => {
-      console.log(ok);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  return request;
 };
 
-checkIfItsDone();
+// 'userid777'
+export { requestUserData };
+
+// requestUserData('user-1').then(data => console.log(data));
+// requestUserData('broken').catch(error => console.log(error));
+// requestUserData('user-2').finally(() => console.log('finally'));
+
+// // option-1
+// requestUserData('user-2')
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('finally'));
+
+// // option-2
+// requestUserData('user-2')
+//   .then(
+//     data => console.log(data),
+//     error => console.log(error),
+//   )
+//   // .catch(error => console.log(error))
+//   .finally(() => console.log('finally'));
+
+// // how to throw Error
+// requestUserData('broken')
+//   .catch(error => {
+//     console.log(error);
+//     throw new Error('Error');
+//   })
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('finally'));
