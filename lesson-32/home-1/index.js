ef66091582c98575d8a34564737bbafc110bd90a
VitaliiHryb <vitaliigryb@ukr.net>
2022-07-28 19:46:02 +0300

    Converter to promise

--- lesson-32/home-1/index.js
+++ lesson-32/home-1/index.js
@@ -1,43 +1,32 @@
-// Фикс цепочки промисов
-const successPromise = new Promise(resolve => {
-  resolve(32);
+// Преобразователь в промис
+
+// input: number
+// output: Promise()
+
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+const makePromise = num => {
+  return new Promise(resolve => {
+    resolve(num);
+  });
+};
+
+// draft solution
+// const makePromise = num => {
+//   const p = new Promise(resolve => {
+//     resolve(num);
+//   });
+
+//   return p;
+// };
+
+/*
+ * пример использования
+ */
+makePromise(17).then(number => {
+  console.log(number); // 17
 });
 
-/*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
- */
-
-successPromise
-  .then(number => {
-    const halfNumber = number / 2;
-    return halfNumber;
-  })
-  .then(
-    number =>
-      new Promise(resolve => {
-        const squaredNumber = number * number;
-        resolve(squaredNumber);
-      }),
-  )
-  .then(result => {
-    console.log(result); // 256
-  });
-
-/*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
- */
-successPromise
-  .then(
-    number =>
-      new Promise(resolve => {
-        const tenNumber = number * 10;
-        resolve(tenNumber);
-      }),
-  )
-  .then(result => {
-    console.log(result); // 320
-  });
-
-console.log(
-  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
-);
+// export { makePromise };
