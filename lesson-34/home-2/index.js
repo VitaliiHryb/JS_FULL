f5627aab62ca735ac624dc0a5443f4d953695ffa
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-04 17:15:29 +0300

    option2

--- lesson-34/home-2/index.js
+++ lesson-34/home-2/index.js
@@ -1,14 +1,6 @@
-// Основные требования:
-// 1. По нажатию на кнопку Register нужно сохранить данные юзера, отправив данные формы на сервер
-// '.submit-button' ==> // (используй в качестве сервера свой mockapi.io)
-// 2. Ответ от сервера требуется вывести в alert в виде объекта
-// 3. После успешного сохранения данных нужно очистить поля формы
-// Подсказка: (установить пустую строку как значение)
-// 4. Кнопка Register должна быть disabled, если хотя бы одно поле не валидно
-// Подсказка: (используй HTMLFormElement.reportValidity() у элемента формы для ее валидации)!
-// 5. После написания и тестирования кода, обязательно проделай рефакторинг (!)
-
 const baseUrl = 'https://62e9354b01787ec712138da8.mockapi.io/api/v1/users';
+
+// const preferDefaultExport = require('eslint-plugin-import/lib/rules/prefer-default-export');
 
 const submitBtnElem = document.querySelector('.submit-button');
 const form = document.forms[0];
@@ -51,3 +43,48 @@ function submitData(event) {
 
 form.addEventListener('input', validateFields);
 form.addEventListener('submit', submitData);
+
+// Основные требования:
+// 1. По нажатию на кнопку Register нужно сохранить данные юзера, отправив данные формы на сервер
+// '.submit-button' ==> // (используй в качестве сервера свой mockapi.io)
+// 2. Ответ от сервера требуется вывести в alert в виде объекта
+// 3. После успешного сохранения данных нужно очистить поля формы
+// Подсказка: (установить пустую строку как значение)
+// 4. Кнопка Register должна быть disabled, если хотя бы одно поле не валидно
+// Подсказка: (используй HTMLFormElement.reportValidity() у элемента формы для ее валидации)!
+
+// const preferDefaultExport = require('eslint-plugin-import/lib/rules/prefer-default-export');
+
+// const baseUrl = 'https://62e9354b01787ec712138da8.mockapi.io/api/v1/users';
+
+// const submitBtnElem = document.querySelector('.submit-button');
+// // const form = document.forms[0];
+// const form = document.forms[0];
+// const inputs = [...document.querySelectorAll('input')];
+// const errorTextElem = document.querySelector('.error-text');
+
+// function validateFields() {
+//   if (form.reportValidity()) submitBtnElem.disabled = false;
+// }
+
+// function submitData(event) {
+//   event.preferDefault();
+
+//   const payload = new FormData(form);
+
+//   alert([...payload]);
+
+//   fetch(baseUrl, {
+//     method: 'POST',
+//     /* headers: {
+//       'Content-Type': 'application/json;charset=utf-8',
+//     }, */
+//     body: payload,
+//   })
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log('Oh Shit, Hier we go again'));
+// }
+
+// form.addEventListener('input', validateFields, true);
+// form.addEventListener('submit', submitData);
