3d1a219a303f8cb9fe2deb41c1de0a0445ccf069
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-05 01:14:36 +0300

    option 2

--- lesson-34/home-2/index.js
+++ lesson-34/home-2/index.js
@@ -1,48 +1,48 @@
 const baseUrl = 'https://62e9354b01787ec712138da8.mockapi.io/api/v1/users';
 
-const preferDefaultExport = require('eslint-plugin-import/lib/rules/prefer-default-export');
+// const preferDefaultExport = require('eslint-plugin-import/lib/rules/prefer-default-export');
 
-const submitBtnElem = document.querySelector('.submit-button');
-const form = document.forms[0];
-const inputs = [...document.querySelectorAll('input')];
-const errorTextElem = document.querySelector('.error-text');
+// const submitBtnElem = document.querySelector('.submit-button');
+// const form = document.forms[0];
+// const inputs = [...document.querySelectorAll('input')];
+// const errorTextElem = document.querySelector('.error-text');
 
-function validateFields() {
-  if (form.reportValidity()) {
-    submitBtnElem.disabled = false;
-  } else {
-    submitBtnElem.disabled = true;
-  }
-  errorTextElem.textContent = '';
-}
+// function validateFields() {
+//   if (form.reportValidity()) {
+//     submitBtnElem.disabled = false;
+//   } else {
+//     submitBtnElem.disabled = true;
+//   }
+//   errorTextElem.textContent = '';
+// }
 
-function submitData(event) {
-  event.preventDefault();
-  const newUser = [...new FormData(form)].reduce(
-    (acc, [field, value]) => ({ ...acc, [field]: value }),
-    {},
-  );
+// function submitData(event) {
+//   event.preventDefault();
+//   const newUser = [...new FormData(form)].reduce(
+//     (acc, [field, value]) => ({ ...acc, [field]: value }),
+//     {},
+//   );
 
-  fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(newUser),
-  })
-    .then(response => response.json())
-    .then(data => {
-      inputs.map(elem => (elem.value = ''));
-      submitBtnElem.disabled = true;
-      alert(JSON.stringify(data));
-    })
-    .catch(() => {
-      errorTextElem.textContent = 'Failed to create user';
-    });
-}
+//   fetch(baseUrl, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json;charset=utf-8',
+//     },
+//     body: JSON.stringify(newUser),
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       inputs.map(elem => (elem.value = ''));
+//       submitBtnElem.disabled = true;
+//       alert(JSON.stringify(data));
+//     })
+//     .catch(() => {
+//       errorTextElem.textContent = 'Failed to create user';
+//     });
+// }
 
-form.addEventListener('input', validateFields);
-form.addEventListener('submit', submitData);
+// form.addEventListener('input', validateFields);
+// form.addEventListener('submit', submitData);
 
 // Основные требования:
 // 1. По нажатию на кнопку Register нужно сохранить данные юзера, отправив данные формы на сервер
@@ -58,34 +58,34 @@ form.addEventListener('submit', submitData);
 // const baseUrl = 'https://62e9354b01787ec712138da8.mockapi.io/api/v1/users';
 
 // OPTION 2
-// const submitBtnElem = document.querySelector('.submit-button');
-// // const form = document.forms[0];
-// const form = document.forms[0];
-// const inputs = [...document.querySelectorAll('input')];
-// const errorTextElem = document.querySelector('.error-text');
+const submitBtnElem = document.querySelector('.submit-button');
+const form = document.querySelector('.login-form');
+const inputs = [...document.querySelectorAll('input')];
+const errorTextElem = document.querySelector('.error-text');
 
-// function validateFields() {
-//   if (form.reportValidity()) submitBtnElem.disabled = false;
-// }
+function validateFields() {
+  // if (form.reportValidity()) submitBtnElem.disabled = false;
+  // submitBtnElem.setAttribute('disabled', !form.reportValidity());
+  submitBtnElem.disabled = !form.reportValidity();
+}
 
-// function submitData(event) {
-//   event.preferDefault();
+function submitData(event) {
+  event.preventDefault();
 
-//   const payload = new FormData(form);
+  const payload = new FormData(form);
 
-//   alert([...payload]);
+  fetch(baseUrl, {
+    method: 'POST',
+    /* headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    }, */
+    body: payload,
+  })
+    .then(res => res.json())
+    .then(data => alert(JSON.stringify(data)))
+    .then(() => form.reset())
+    .catch(err => console.log('Oh Shit, Here we go again'));
+}
 
-//   fetch(baseUrl, {
-//     method: 'POST',
-//     /* headers: {
-//       'Content-Type': 'application/json;charset=utf-8',
-//     }, */
-//     body: payload,
-//   })
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-//     .catch(err => console.log('Oh Shit, Hier we go again'));
-// }
-
-// form.addEventListener('input', validateFields, true);
-// form.addEventListener('submit', submitData);
+form.addEventListener('input', validateFields);
+form.addEventListener('submit', submitData);
