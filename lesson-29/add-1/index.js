9daf864ce5c948320c7ce03ecb2209aa1c00fc49
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-19 22:36:55 +0300

    Функция для задержки вызова

---
+++ lesson-29/add-1/index.js
@@ -0,0 +1,31 @@
+// Функция для задержки вызова
+
+// Напишите функцию,
+// которая выполнит другую ф-цию с определенной задержкой
+
+// Основные требования:
+// 1. Файл должен экспортировать функцию под именем delay
+// 2. Сигнатура: delay(delay, callback, context, arg1, arg2, arg3, ...),
+// где delay - время задержки (число), callback - ф-ция, которая будет вызвана,
+// context - контекст для вызова callback,
+// arg1, arg2, arg3, ... - аргументы для вызова callback (может быть неограниченное к-во)
+
+'use strict';
+
+const delay = (delay, callback, context, ...args) => {
+  setTimeout(() => {
+    callback.bind(context)(...args);
+  }, delay);
+};
+
+// export { delay };
+
+// const test1 = (...rest) => alert(`Привет ${[...rest][1]}`);
+
+// const test2 = (...rest) => {
+//   for (let i = 0; i < [...rest].length; i += 1) {
+//     console.log(`Привет ${[...rest][i]}`);
+//   }
+// };
+
+// delay(3000, test2, 'Tom', 'Jack', 'John', 'Jinx');
