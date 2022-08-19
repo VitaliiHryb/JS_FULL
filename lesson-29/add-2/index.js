4e756962f1db20c7002c632408625c98fc55f9f5
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-20 00:33:11 +0300

    Load user data

--- lesson-29/add-2/index.js
+++ lesson-29/add-2/index.js
@@ -10,32 +10,80 @@
 // Напиши функцию, которая принимает id пользователя
 // и имитирует загрузку данных о пользователе
 
+// option-1
 const requestUserData = (userId, callback) => {
-  const randomNumber = Math.floor(Math.random() * 4000);
+  const randomNumber = Math.floor(1000 + Math.random() * 2000);
+  // console.log(randomNumber);
 
   if (userId === 'broken') {
-    setTimeout(callback(null, 'Failed to load user data'), randomNumber);
+    setTimeout(() => callback(null, 'Failed to load user data'), randomNumber);
   } else {
-    setTimeout(callback(userId), randomNumber);
+    setTimeout(
+      () => callback(userId, { userId, email: `${userId}@example.com` }),
+      randomNumber,
+    );
   }
 };
 
-export { requestUserData };
+// export { requestUserData };
 
-// // test data
+// test data
 
-// requestUserData('John', (userId, err) =>
-//   console.log(
-//     userId === 'broken'
-//       ? err
-//       : `{ userId: ${userId}, email: '${userId}@example.com' }`,
-//   ),
-// );
+requestUserData('John', (userId, isErr) => console.log(isErr));
 
-// requestUserData('broken', (userId, err) =>
-//   console.log(
-//     userId === null
-//       ? err
-//       : `{ userId: ${userId}, email: '${userId}@example.com' }`,
-//   ),
-// );
+requestUserData('broken', (userId, isErr) => console.log(isErr));
+
+// option-2
+// const requestUserData = (userId, callback) => {
+//   const randomNumber = Math.floor(1000 + Math.random() * 2000);
+//   // console.log(randomNumber);
+
+//   if (userId === 'broken') {
+//     setTimeout(() => callback(null, 'Failed to load user data'), randomNumber);
+//   } else {
+//     setTimeout(
+//       () =>
+//         callback(
+//           userId,
+//           `{ userId: ${userId}, email: '${userId}@example.com' }`,
+//         ),
+//       randomNumber,
+//     );
+//   }
+// };
+
+// requestUserData('John', (userId, isErr) => console.log(isErr));
+
+// requestUserData('broken', (userId, isErr) => console.log(isErr));
+
+// option-3
+// const requestUserData = (userId, callback) => {
+//   const randomNumber = Math.floor(1000 + Math.random() * 2000);
+//   // console.log(randomNumber);
+
+//   if (userId === 'broken') {
+//     setTimeout(() => callback(null, 'Failed to load user data'), randomNumber);
+//   } else {
+//     setTimeout(() => callback(userId), randomNumber);
+//   }
+// };
+
+// export { requestUserData };
+
+// // // test data
+
+// // requestUserData('John', (userId, err) =>
+// //   console.log(
+// //     userId === null
+// //       ? err
+// //       : `{ userId: ${userId}, email: '${userId}@example.com' }`,
+// //   ),
+// // );
+
+// // requestUserData('broken', (userId, err) =>
+// //   console.log(
+// //     userId === null
+// //       ? err
+// //       : `{ userId: ${userId}, email: '${userId}@example.com' }`,
+// //   ),
+// // );
