1dfde1af6c96928673ec0728f883aa4ce07b6330
VitaliiHryb <vitaliigryb@ukr.net>
2022-08-20 14:39:25 +0300

    Promisification

--- lesson-29/add-2/index.js
+++ lesson-29/add-2/index.js
@@ -15,12 +15,13 @@ const addImageV2 = url => {
 export { addImageV2 };
 
 // examples
-
+/*
 addImageV2(
   'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
 )
   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
   .catch(error => console.log(error)); // ==> 'Image load failed'
+*/
 
 // addImageV2('https://server.com/image.png')
 //   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
